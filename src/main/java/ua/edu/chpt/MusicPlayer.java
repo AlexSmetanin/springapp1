f2f6973fcf9e5bffb8eabdc969ca03f0e9871c6f
AlexSmetanin <AlexSmetanin72@gmail.com>
2022-02-05 00:07:36 +0200

    Anotation Autowired part1-2

--- src/main/java/ua/edu/chpt/MusicPlayer.java
+++ src/main/java/ua/edu/chpt/MusicPlayer.java
@@ -1,39 +1,18 @@
 package ua.edu.chpt;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MusicPlayer {
-    private Music music;
-    private String name;
-    private int volume;
+    private ClassicalMusic classicalMusic;
 
-    // Inversion of Control
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public MusicPlayer() {
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic) {
+        this.classicalMusic = classicalMusic;
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        System.out.println("Playing: " + classicalMusic.getSong());
     }
 }
